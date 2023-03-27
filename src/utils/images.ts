523708bcf08d5625c51d2c03b19e39828f95df81
Anton Pashkovskyi <anton@globalbit.co.il>
2023-03-27 22:12:18 +0300

    fix image resizing

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -6,5 +6,6 @@ export const resizeBuffer = async (buffer: Buffer): Promise<Buffer> => {
   const sharp = await getSharp();
   return sharp(buffer)
     .resize(512)
+    .withMetadata()
     .toBuffer();
 };
