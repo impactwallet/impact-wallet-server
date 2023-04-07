c276017b6b22e2bcc55c42b81e31cdbf54c39eb7
VitkoDmitriy <vitko.dmitriy@gmail.com>
2023-04-07 14:05:54 +0300

    hide telegram keys

--- src/api-service/api.module.ts
+++ src/api-service/api.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { CandyPayService } from './candypay.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule],
   providers: [ApiService, CandyPayService],
   exports: [ApiService, CandyPayService],
   controllers: [ApiController],
