bc704f4a76ebc6002e102cd4001cec7d902d876c
Anton Pashkovskyi <anton@globalbit.co.il>
2023-06-25 22:01:35 +0200

    add orgUser api prop to member dto

--- src/members/dto/members.dto.ts
+++ src/members/dto/members.dto.ts
@@ -33,6 +33,9 @@ export class MemberDto {
   @ApiProperty({ example: '0b1bd52d-7d8e-4518-b0a3-13ae5ad52d47', description: 'User id' })
     user: string;
 
+  @ApiProperty({ example: '0b1bd52d-7d8e-4518-b0a3-13ae5ad52d47', description: 'Org user id' })
+    orgUser: string;
+
   org: string;
 
   @ApiProperty({ type: InvestorSettingsDto, description: 'Investor settings' })
--- src/orgs/orgs.service.lite.ts
+++ src/orgs/orgs.service.lite.ts
@@ -23,11 +23,13 @@ export class OrgsServiceLite {
 
 
   async createOrgLite(orgsDto: CreateOrgDto, logo: any, mock: boolean, req: Request) {
-    const user = await this.authService.getAccountFromToken(req);
+    const account = await this.authService.getAccountFromToken(req);
+    const userField = account.isUser ? 'user' : 'orgUser';
     orgsDto.member.equity = { amount: 100, type: EquityType.Immediately };
+    orgsDto.member[userField] = account.id.toString();
 
     const { org, member } = await this.orgsService.createOrganization(orgsDto, logo, mock);
-    const initialMint = { wallet: user.wallet, amount: 100 * LAMPORTS_PER_SOL };
+    const initialMint = { wallet: account.wallet, amount: 100 * LAMPORTS_PER_SOL };
     this.orgsService.createToken(org, initialMint)
       .then(() => {
         this.memberService.updateContributed(member._id, 0, initialMint.amount).exec();
