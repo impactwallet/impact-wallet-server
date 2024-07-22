16197a76ee1c0e3f1c5fc9d18767def429bb344d
Anton Pashkovskyi <anton@globalbit.co.il>
2024-07-22 14:24:54 +0200

    update airdrop periods

--- src/airdrop/airdrop.service.ts
+++ src/airdrop/airdrop.service.ts
@@ -60,8 +60,8 @@ export class AirdropService {
   }
 
   async calculate(excludeWalletsMap: any) {
-    const holdPeriodStartDate = 1719705600;
-    const holdPeriodEndDate = 1720396799;
+    const holdPeriodStartDate = 1720396800;
+    const holdPeriodEndDate = 1721001599;
 
     //Get all our token holders
     let orgHolders = await this.apiService.getTokenHolders(
@@ -502,10 +502,10 @@ export class AirdropService {
     const tzOffsetSeconds = tzOffset * 60;
     return {
       holdFromDate: 1711155600 + tzOffsetSeconds,
-      holdToDate: 1721001599 + tzOffsetSeconds,
-      claimFromDate: 1721001600 + tzOffsetSeconds,
-      claimToDate: 1721606399 + tzOffsetSeconds,
-      round: 7, // TODO: increment
+      holdToDate: 1721606399 + tzOffsetSeconds,
+      claimFromDate: 1721606400 + tzOffsetSeconds,
+      claimToDate: 1722211199 + tzOffsetSeconds,
+      round: 8, // TODO: increment
     };
   }
 
